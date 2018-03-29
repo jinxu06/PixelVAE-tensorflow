6ffde67df8a7aa46f5cad40564a44876229f667f
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-29 22:40:01 +0800

    sync

--- test_pixelvae.py
+++ test_pixelvae.py
@@ -227,4 +227,4 @@ with tf.Session(config=config) as sess:
 
     img_tile = plotting.img_tile(sample_x[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
     img = plotting.plot_img(img_tile, title=args.data_set + ' samples')
-    plotting.plt.savefig(os.path.join(args.save_dir,'%s_pixelvae_sample%d.png' % (args.data_set, epoch)))
+    plotting.plt.savefig(os.path.join(args.save_dir,'%s_pixelvae_sample.png' % (args.data_set)))
