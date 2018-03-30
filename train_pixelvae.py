a85fff4a9b3301a60527bea7536d7d8fc85196e2
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-30 20:09:27 +0800

    sync

--- train_pixelvae.py
+++ train_pixelvae.py
@@ -235,7 +235,6 @@ with tf.Session(config=config) as sess:
             loss_arr.append(l)
             nll_arr.append(n)
             kld_arr.append(k)
-            print(l, n, k)
         train_loss, train_nll, train_kld = np.mean(loss_arr), np.mean(nll_arr), np.mean(kld_arr)
 
         loss_arr, nll_arr, kld_arr = [], [], []
