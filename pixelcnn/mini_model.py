a3e58baafcf16a476984f7b1e410b6fa129e3939
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-30 17:28:25 +0800

    sync

--- pixelcnn/mini_model.py
+++ pixelcnn/mini_model.py
@@ -27,6 +27,7 @@ def shallow_pixel_cnn(x, gh=None, sh=None, init=False, ema=None, dropout_p=0.5, 
                 for rep in range(nr_resnet):
                     u_list.append(nn.gated_resnet(u_list[-1], conv=nn.down_shifted_conv2d))
                     ul_list.append(nn.gated_resnet(ul_list[-1], u_list[-1], conv=nn.down_right_shifted_conv2d))
+                    
                     # if rep%2==0:
                     #     for k in range(2):
                     #         u_list[-1] = nn.nin(u_list[-1], nn.int_shape(u_list[-1])[-1])
