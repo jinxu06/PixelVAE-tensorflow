92dfdf6c55cfeca8e67268d2a6234e430c1538ea
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-03 23:06:48 +0800

    sync

--- train_lvae.py
+++ train_lvae.py
@@ -17,6 +17,7 @@ cfg = {
     "data_set": "celeba32",
     "batch_size": 8,
     "nr_gpu": 1,
+    "learning_rate": 0.0001,
 }
 
 parser.add_argument('-is', '--img_size', type=int, default=cfg['img_size'], help="size of input image")
@@ -28,6 +29,7 @@ parser.add_argument('-ds', '--data_set', type=str, default=cfg['data_set'], help
 #parser.add_argument('-lp', '--load_params', dest='load_params', action='store_true', help='Restore training from previous model checkpoint?')
 parser.add_argument('-bs', '--batch_size', type=int, default=cfg['batch_size'], help='Batch size during training per GPU')
 parser.add_argument('-ng', '--nr_gpu', type=int, default=cfg['nr_gpu'], help='How many GPUs to distribute the training across?')
+parser.add_argument('-lr', '--learning_rate', type=float, default=cfg['learning_rate'], help='Base learning rate')
 parser.add_argument('-s', '--seed', type=int, default=1, help='Random seed to use')
 # new features
 parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Under debug mode?')
@@ -59,7 +61,22 @@ model = tf.make_template('build_graph', VLadderAE.build_graph)
 for i in range(args.nr_gpu):
     with tf.device('/gpu:%d' % i):
         model(vladders[i], xs[i])
-        print(vladders[i].zs)
+
+all_params = tf.trainable_variables()
+
+for i in range(args.nr_gpu):
+    with tf.device('/gpu:%d' % i):
+        grads[i] = tf.gradients(vladders[i].loss, all_params, colocate_gradients_with_ops=True)
+
+with tf.device('/gpu:0'):
+    for i in range(1, args.nr_gpu):
+        for j in range(len(grads[0])):
+            grads[0][j] += grads[i][j]
+
+    loss = tf.addn([v.loss for v in vladders]) / args.nr_gpu
+    loss_ae = tf.addn([v.loss_ae for v in vladders]) / args.nr_gpu
+    loss_reg = tf.addn([v.loss_reg for v in vladders]) / args.nr_gpu
+    train_step = adam_updates(all_params, grads[0], lr=args.learning_rate)
 
 
 
@@ -78,4 +95,22 @@ config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
 
     sess.run(initializer)
-    print(vladder)
+
+
+
+    max_num_epoch = 1000
+    for epoch in range(max_num_epoch):
+        tt = time.time()
+        loss_arr, loss_ae_arr, loss_reg_arr = [], [], []
+        for data in train_data:
+            feed_dict = make_feed_dict(data)
+            l, la, lr, _ = sess.run([loss, loss_ae, loss_reg, train_step], feed_dict=feed_dict)
+            loss_arr.append(l)
+            loss_ae_arr.append(la)
+            loss_reg_arr.append(lr)
+
+        train_loss, train_loss_ae, train_loss_reg = np.mean(loss_arr), np.mean(loss_ae_arr), np.mean(loss_reg_arr)
+
+        print("epoch {0} --------------------- Time {1:.2f}s".format(epoch, time.time()-tt))
+        print("train loss:{0:.3f}, train nll:{1:.3f}, train kld:{2:.6f}".format(train_loss, train_loss_ae, train_loss_reg))
+        sys.stdout.flush()
