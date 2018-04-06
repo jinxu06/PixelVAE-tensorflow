c944468dc5db3d60683dae51095342cbf1fd591a
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-06 11:37:36 +0800

    sync

--- test_conv_vae.py
+++ test_conv_vae.py
@@ -154,7 +154,7 @@ with tf.Session(config=config) as sess:
     saver.save(sess, args.save_dir + '/params_' + args.data_set + '.ckpt')
     data = next(test_data)
     for i in range(data.shape[0]):
-        data[i] = data[0].copy()
+        data[i] = data[1].copy()
     sample_x = generate_samples(sess, data)
     test_data.reset()
 
