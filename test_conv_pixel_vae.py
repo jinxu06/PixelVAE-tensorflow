767cc03718d0b6344bffcc3e313104c005732cc9
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-09 11:25:56 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -12,23 +12,22 @@ from layers import visualize_samples
 
 parser = argparse.ArgumentParser()
 
-cfg = {
-    "img_size": 32,
-    "z_dim": 32,
-    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
-    "data_set": "celeba32",
-    "batch_size": 80,
-    "nr_gpu": 4,
-    #"gpus": "4,5,6,7",
-    "learning_rate": 0.0001,
-    "beta": 1,
-    "lam": 0.0,
-    "save_interval": 10,
-    #"reg": "tc-dwkld",
-    "reg": "kld",
-    "use_mode": "test",
-}
+# cfg = {
+#     "img_size": 32,
+#     "z_dim": 32,
+#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_tc-dwkld",
+#     "data_set": "celeba32",
+#     "batch_size": 80,
+#     "nr_gpu": 4,
+#     #"gpus": "4,5,6,7",
+#     "learning_rate": 0.0001,
+#     "beta": 1,
+#     "lam": 0.0,
+#     "save_interval": 10,
+#     "reg": "tc-dwkld",
+#     "use_mode": "test",
+# }
 
 # cfg = {
 #     "img_size": 32,
@@ -44,25 +43,25 @@ cfg = {
 #     "lam": 0.0,
 #     "save_interval": 10,
 #     "reg": "mmd",
-#     "use_mode": "train",
+#     "use_mode": "test",
 # }
 
-# cfg = {
-#     "img_size": 32,
-#     "z_dim": 32,
-#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
-#     "data_set": "celeba32",
-#     "batch_size": 32,
-#     "nr_gpu": 4,
-#     #"gpus": "4,5,6,7",
-#     "learning_rate": 0.0001,
-#     "beta": 1,
-#     "lam": 0.0,
-#     "save_interval": 10,
-#     "reg": "kld",
-#     "use_mode": "train",
-# }
+cfg = {
+    "img_size": 32,
+    "z_dim": 32,
+    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
+    "data_set": "celeba32",
+    "batch_size": 32,
+    "nr_gpu": 4,
+    #"gpus": "4,5,6,7",
+    "learning_rate": 0.0001,
+    "beta": 1,
+    "lam": 0.0,
+    "save_interval": 10,
+    "reg": "kld",
+    "use_mode": "test",
+}
 
 
 
