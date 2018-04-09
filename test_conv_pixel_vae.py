1cc6a6d5f6db82eaf15114ffa6869f1edd5df5b0
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-09 11:39:27 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -29,39 +29,39 @@ parser = argparse.ArgumentParser()
 #     "use_mode": "test",
 # }
 
-# cfg = {
-#     "img_size": 32,
-#     "z_dim": 32,
-#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_mmd",
-#     "data_set": "celeba32",
-#     "batch_size": 80,
-#     "nr_gpu": 4,
-#     #"gpus": "4,5,6,7",
-#     "learning_rate": 0.0001,
-#     "beta": 1e5,
-#     "lam": 0.0,
-#     "save_interval": 10,
-#     "reg": "mmd",
-#     "use_mode": "test",
-# }
-
 cfg = {
     "img_size": 32,
     "z_dim": 32,
     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
+    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_mmd",
     "data_set": "celeba32",
     "batch_size": 80,
     "nr_gpu": 4,
     #"gpus": "4,5,6,7",
     "learning_rate": 0.0001,
-    "beta": 1,
+    "beta": 1e5,
     "lam": 0.0,
     "save_interval": 10,
-    "reg": "kld",
+    "reg": "mmd",
     "use_mode": "test",
 }
+
+# cfg = {
+#     "img_size": 32,
+#     "z_dim": 32,
+#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
+#     "data_set": "celeba32",
+#     "batch_size": 80,
+#     "nr_gpu": 4,
+#     #"gpus": "4,5,6,7",
+#     "learning_rate": 0.0001,
+#     "beta": 1,
+#     "lam": 0.0,
+#     "save_interval": 10,
+#     "reg": "kld",
+#     "use_mode": "test",
+# }
 
 
 
@@ -265,4 +265,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("results/conv_pixel_vae_samples_kld.png")
+    img.save("results/conv_pixel_vae_samples_mmd.png")
