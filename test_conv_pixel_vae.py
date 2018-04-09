a7dcd65cd8d5fac7b799db2a135337ad30ee77c9
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-09 12:20:59 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -12,39 +12,39 @@ from layers import visualize_samples
 
 parser = argparse.ArgumentParser()
 
-# cfg = {
-#     "img_size": 32,
-#     "z_dim": 32,
-#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_tc-dwkld",
-#     "data_set": "celeba32",
-#     "batch_size": 80,
-#     "nr_gpu": 4,
-#     #"gpus": "4,5,6,7",
-#     "learning_rate": 0.0001,
-#     "beta": 1,
-#     "lam": 0.0,
-#     "save_interval": 10,
-#     "reg": "tc-dwkld",
-#     "use_mode": "test",
-# }
-
 cfg = {
     "img_size": 32,
     "z_dim": 32,
     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_mmd",
+    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_tc-dwkld",
     "data_set": "celeba32",
     "batch_size": 80,
     "nr_gpu": 4,
     #"gpus": "4,5,6,7",
     "learning_rate": 0.0001,
-    "beta": 1e5,
+    "beta": 1,
     "lam": 0.0,
     "save_interval": 10,
-    "reg": "mmd",
+    "reg": "tc-dwkld",
     "use_mode": "test",
 }
+
+# cfg = {
+#     "img_size": 32,
+#     "z_dim": 32,
+#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_mmd",
+#     "data_set": "celeba32",
+#     "batch_size": 80,
+#     "nr_gpu": 4,
+#     #"gpus": "4,5,6,7",
+#     "learning_rate": 0.0001,
+#     "beta": 1e5,
+#     "lam": 0.0,
+#     "save_interval": 10,
+#     "reg": "mmd",
+#     "use_mode": "test",
+# }
 
 # cfg = {
 #     "img_size": 32,
@@ -216,12 +216,12 @@ def latent_traversal(sess, data, use_image_id=0):
     num_features = 32
     num_traversal_step = 10
     for i in range(num_features):
-        z[i*num_traversal_step:(i+1)*num_traversal_step, i] = np.linspace(start=-5., stop=5., num=num_traversal_step)
+        z[i*num_traversal_step:(i+1)*num_traversal_step, i] = np.linspace(start=-1., stop=1., num=num_traversal_step)
     z = np.split(z, args.nr_gpu)
     feed_dict.update({pvaes[i].z:z[i] for i in range(args.nr_gpu)})
 
     x_gen = [ds[i].copy() for i in range(args.nr_gpu)]
-    for yi in range(16, args.img_size):
+    for yi in range(args.img_size):
         for xi in range(args.img_size):
             print(yi, xi)
             feed_dict.update({x_bars[i]:x_gen[i] for i in range(args.nr_gpu)})
@@ -265,4 +265,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("results/conv_pixel_vae_completion_mmd.png")
+    img.save("results/conv_pixel_vae_tc-dwkld.png")
