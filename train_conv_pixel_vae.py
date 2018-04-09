42e1b5733b0cdca53407ddcadcec6afcfcb6671e
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-09 21:43:36 +0800

    sync

--- train_conv_pixel_vae.py
+++ train_conv_pixel_vae.py
@@ -47,39 +47,39 @@ parser = argparse.ArgumentParser()
 # }
 
 
-# cfg = {
-#     "img_size": 32,
-#     "z_dim": 32,
-#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_mmd",
-#     "data_set": "celeba32",
-#     "batch_size": 32,
-#     "nr_gpu": 4,
-#     #"gpus": "4,5,6,7",
-#     "learning_rate": 0.0001,
-#     "beta": 1e5,
-#     "lam": 0.0,
-#     "save_interval": 10,
-#     "reg": "mmd",
-#     "use_mode": "train",
-# }
-
 cfg = {
     "img_size": 32,
     "z_dim": 32,
     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
+    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_mmd",
     "data_set": "celeba32",
     "batch_size": 32,
     "nr_gpu": 4,
     #"gpus": "4,5,6,7",
     "learning_rate": 0.0001,
-    "beta": 1,
+    "beta": 1e5,
     "lam": 0.0,
     "save_interval": 10,
-    "reg": "kld",
+    "reg": "mmd",
     "use_mode": "train",
 }
+
+# cfg = {
+#     "img_size": 32,
+#     "z_dim": 32,
+#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
+#     "data_set": "celeba32",
+#     "batch_size": 32,
+#     "nr_gpu": 4,
+#     #"gpus": "4,5,6,7",
+#     "learning_rate": 0.0001,
+#     "beta": 1,
+#     "lam": 0.0,
+#     "save_interval": 10,
+#     "reg": "kld",
+#     "use_mode": "train",
+# }
 
 
 
