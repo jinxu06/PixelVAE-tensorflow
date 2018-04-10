f40f830ca5e0da8b9dbc6e7e7e49cadcf4871227
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-10 11:17:41 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -45,22 +45,22 @@ parser = argparse.ArgumentParser()
 #     "use_mode": "test",
 # }
 
-# cfg = {
-#     "img_size": 32,
-#     "z_dim": 32,
-#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_tc-dwkld",
-#     "data_set": "celeba32",
-#     "batch_size": 80,
-#     "nr_gpu": 4,
-#     #"gpus": "4,5,6,7",
-#     "learning_rate": 0.0001,
-#     "beta": 1,
-#     "lam": 0.0,
-#     "save_interval": 10,
-#     "reg": "tc-dwkld",
-#     "use_mode": "test",
-# }
+cfg = {
+    "img_size": 32,
+    "z_dim": 32,
+    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_tc_beta8",
+    "data_set": "celeba32",
+    "batch_size": 32,
+    "nr_gpu": 4,
+    #"gpus": "4,5,6,7",
+    "learning_rate": 0.0001,
+    "beta": 8,
+    "lam": 0.0,
+    "save_interval": 10,
+    "reg": "tc",
+    "use_mode": "train",
+}
 
 # cfg = {
 #     "img_size": 32,
@@ -79,22 +79,22 @@ parser = argparse.ArgumentParser()
 #     "use_mode": "test",
 # }
 
-cfg = {
-    "img_size": 32,
-    "z_dim": 32,
-    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
-    "data_set": "celeba32",
-    "batch_size": 80,
-    "nr_gpu": 4,
-    #"gpus": "4,5,6,7",
-    "learning_rate": 0.0001,
-    "beta": 1,
-    "lam": 0.0,
-    "save_interval": 10,
-    "reg": "kld",
-    "use_mode": "test",
-}
+# cfg = {
+#     "img_size": 32,
+#     "z_dim": 32,
+#     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+#     "save_dir": "/data/ziz/jxu/models/conv_pixel_vae_celeba32_kld",
+#     "data_set": "celeba32",
+#     "batch_size": 80,
+#     "nr_gpu": 4,
+#     #"gpus": "4,5,6,7",
+#     "learning_rate": 0.0001,
+#     "beta": 1,
+#     "lam": 0.0,
+#     "save_interval": 10,
+#     "reg": "kld",
+#     "use_mode": "test",
+# }
 
 
 
@@ -297,4 +297,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("results/conv_pixel_vae_kld.png")
+    img.save("results/conv_pixel_vae_tc_beta8.png")
