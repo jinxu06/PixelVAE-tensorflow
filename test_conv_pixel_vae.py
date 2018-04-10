20a4f6b05acace4e54fce31494458ae2977a1fb8
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-10 22:17:23 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -299,29 +299,29 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
 
-    # ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
-    # print('restoring parameters from', ckpt_file)
-    # saver.restore(sess, ckpt_file)
-    #
-    # data = next(test_data)
-    # sample_x = generate_samples(sess, data)
-    # visualize_samples(sample_x, "results/conv_pixel_vae_test.png", layout=(8,8))
-
-
-
     ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
     print('restoring parameters from', ckpt_file)
     saver.restore(sess, ckpt_file)
 
-    data = test_data.next(32*10)
-    test_data.reset()
-    img = []
-    for i in range(3):
-        sample_x = latent_traversal(sess, data, use_image_id=5+i)
-        view = visualize_samples(sample_x, None, layout=(32, 10))
-        img.append(view.copy())
-    img = np.concatenate(img, axis=1)
-    from PIL import Image
-    img = img.astype(np.uint8)
-    img = Image.fromarray(img, 'RGB')
-    img.save("results/conv_pixel_vae_tc-dwkld.png")
+    data = next(test_data)
+    sample_x = generate_samples(sess, data)
+    visualize_samples(sample_x, "results/conv_pixel_vae_test.png", layout=(8,8))
+
+
+
+    # ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
+    # print('restoring parameters from', ckpt_file)
+    # saver.restore(sess, ckpt_file)
+    #
+    # data = test_data.next(32*10)
+    # test_data.reset()
+    # img = []
+    # for i in range(3):
+    #     sample_x = latent_traversal(sess, data, use_image_id=5+i)
+    #     view = visualize_samples(sample_x, None, layout=(32, 10))
+    #     img.append(view.copy())
+    # img = np.concatenate(img, axis=1)
+    # from PIL import Image
+    # img = img.astype(np.uint8)
+    # img = Image.fromarray(img, 'RGB')
+    # img.save("results/conv_pixel_vae_tc-dwkld.png")
