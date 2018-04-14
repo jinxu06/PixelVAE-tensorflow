250d7e8b3c96b3b592248e39ed50c14a8965c065
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-14 22:18:37 +0800

    sync

--- train_conv_pixel_vae.py
+++ train_conv_pixel_vae.py
@@ -162,7 +162,6 @@ def sample_from_model(sess, data):
     for yi in range(args.img_size):
         for xi in range(args.img_size):
             if tm[0, yi, xi]==0:
-                print(yi, xi)
                 feed_dict.update({x_bars[i]:x_gen[i] for i in range(args.nr_gpu)})
                 x_hats = sess.run([pvaes[i].x_hat for i in range(args.nr_gpu)], feed_dict=feed_dict)
                 for i in range(args.nr_gpu):
