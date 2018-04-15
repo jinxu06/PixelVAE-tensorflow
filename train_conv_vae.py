31a16051c7a3214e73f0c1d41f48198f1475b2eb
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-15 23:26:45 +0800

    sync

--- train_conv_vae.py
+++ train_conv_vae.py
@@ -28,39 +28,39 @@ parser = argparse.ArgumentParser()
 #     "use_mode": "train",
 # }
 
-cfg = {
-    "img_size": 64,
-    "z_dim": 32,
-    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/conv_vae_celeba64_tc_beta5",
-    "data_set": "celeba64",
-    "batch_size": 512,
-    "nr_gpu": 2,
-    #"gpus": "4,5,6,7",
-    "learning_rate": 0.001,
-    "beta": 5.0,
-    "lam": 0.0,
-    "save_interval": 10,
-    "reg": "tc",
-    "use_mode": "train",
-}
-#
 # cfg = {
 #     "img_size": 64,
 #     "z_dim": 32,
 #     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-#     "save_dir": "/data/ziz/jxu/models/conv_vae_celeba64_tc_beta10",
+#     "save_dir": "/data/ziz/jxu/models/conv_vae_celeba64_tc_beta5",
 #     "data_set": "celeba64",
 #     "batch_size": 512,
 #     "nr_gpu": 2,
 #     #"gpus": "4,5,6,7",
 #     "learning_rate": 0.001,
-#     "beta": 10.0,
+#     "beta": 5.0,
 #     "lam": 0.0,
 #     "save_interval": 10,
 #     "reg": "tc",
 #     "use_mode": "train",
 # }
+#
+cfg = {
+    "img_size": 64,
+    "z_dim": 32,
+    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+    "save_dir": "/data/ziz/jxu/models/conv_vae_celeba64_tc_beta10",
+    "data_set": "celeba64",
+    "batch_size": 512,
+    "nr_gpu": 2,
+    #"gpus": "4,5,6,7",
+    "learning_rate": 0.001,
+    "beta": 10.0,
+    "lam": 0.0,
+    "save_interval": 10,
+    "reg": "tc",
+    "use_mode": "train",
+}
 #
 # cfg = {
 #     "img_size": 64,
