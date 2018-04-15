5a98c35bb14c674f99f7145694d1a92f849b8bb8
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-15 11:59:19 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -343,7 +343,7 @@ def generate_samples(sess, data):
 
     x_gen = [ds[i].copy() for i in range(args.nr_gpu)]
     x_gen = [x_gen[i]*np.stack([tm for t in range(3)], axis=-1) for i in range(args.nr_gpu)]
-    return np.concatenate(x_gen, axis=0)
+    #return np.concatenate(x_gen, axis=0)
 
     for yi in range(args.img_size):
         for xi in range(args.img_size):
@@ -402,10 +402,10 @@ with tf.Session(config=config) as sess:
     data = next(test_data)
     data = next(test_data)
 
+    #sample_x = generate_samples(sess, data)
+    #visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_no_conditioning_original.png", layout=(10, 10))
     sample_x = generate_samples(sess, data)
-    visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_no_conditioning_original.png", layout=(10, 10))
-    # sample_x = generate_samples(sess, data)
-    # visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_conditioning.png", layout=(10, 10))
+    visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_conditioning.png", layout=(10, 10))
 
 
     # ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
