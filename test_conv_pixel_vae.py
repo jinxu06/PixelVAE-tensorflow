1e8b319e1889e5cb904f1237f9748bc4ff71bb9f
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-15 11:22:44 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -396,8 +396,9 @@ with tf.Session(config=config) as sess:
     saver.restore(sess, ckpt_file)
 
     data = next(test_data)
+    data = next(test_data)
     sample_x = generate_samples(sess, data)
-    visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_no_conditioning.png", layout=(8,8))
+    visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_no_conditioning.png", layout=(10, 10))
 
 
     # ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
