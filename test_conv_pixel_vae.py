c3c67a00c34c82373c179d3e2cc2fd795e4a3e53
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-16 17:16:51 +0800

    sync

--- test_conv_pixel_vae.py
+++ test_conv_pixel_vae.py
@@ -108,7 +108,7 @@ model_opt = {
     "nr_resnet": args.nr_resnet,
     "nr_filters": args.nr_filters,
     "nr_logistic_mix": args.nr_logistic_mix,
-    "sample_range": 3.0,
+    "sample_range": 1.0,
 }
 model = tf.make_template('model', ConvPixelVAE.build_graph)
 
@@ -253,7 +253,7 @@ with tf.Session(config=config) as sess:
     #visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_no_conditioning_original.png", layout=(10, 10))
     sample_x = generate_samples(sess, data)
     #visualize_samples(sample_x, "results/conv_pixel_vae_celeba32_mmd_no_conditioning_original.png", layout=(10, 10))
-    visualize_samples(sample_x, "results/show_4.png", layout=(10, 10))
+    visualize_samples(sample_x, "results/show_5.png", layout=(10, 10))
 
 
     # ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
