fa06785e209ddaa4d58e70a848eeecc02ad53184
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-17 20:04:04 +0800

    sync

--- test_conv_vae.py
+++ test_conv_vae.py
@@ -29,6 +29,23 @@ cfg = {
     "use_mode": "test",
 }
 
+cfg = {
+    "img_size": 64,
+    "z_dim": 16,
+    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+    "save_dir": "/data/ziz/jxu/models/conv_vae_celeba64_tc_beta10_rep",
+    "data_set": "celeba64",
+    "batch_size": 40,
+    "nr_gpu": 2,
+    #"gpus": "4,5,6,7",
+    "learning_rate": 0.0002,
+    "beta": 10.0,
+    "lam": 0.0,
+    "save_interval": 10,
+    "reg": "tc",
+    "use_mode": "test",
+}
+
 
 
 parser.add_argument('-is', '--img_size', type=int, default=cfg['img_size'], help="size of input image")
@@ -184,7 +201,7 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("results/conv_vae_samples_celeba64_sconv.png")
+    img.save("results/conv_vae_samples_celeba64_rep.png")
 
     # data = next(test_data)
     # sample_x = generate_samples(sess, data)
