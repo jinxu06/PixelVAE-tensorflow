c3d5c51e1223219fccd6d8e050bc536777369f7e
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-17 21:12:03 +0800

    sync

--- layers.py
+++ layers.py
@@ -180,14 +180,20 @@ def compute_lse_sum_and_sum_lse(z, z_mu, z_log_sigma_sq):
     z_sigma = tf.sqrt(tf.exp(z_log_sigma_sq))
     log_probs = []
     batch_size, z_dim = int_shape(z_mu)
-    z_b = tf.stack([z for i in range(batch_size)], axis=0)
+    z_b = tf.stack([z for i in range(batch_size-1)], axis=0)
     z_mu_b = tf.stack([z_mu for i in range(batch_size)], axis=1)
     z_sigma_b = tf.stack([z_sigma for i in range(batch_size)], axis=1)
     z_norm = (z_b-z_mu_b) / z_sigma_b
 
     dist = tf.distributions.Normal(loc=0., scale=1.)
     log_probs = dist.log_prob(z_norm)
-    return log_sum_exp(tf.reduce_sum(log_probs, axis=-1), axis=0)
+    a = log_probs
+    print(int_shape(a))
+    a = tf.reduce_sum(a, axis=-1)
+    print(int_shape(a))
+    a = log_sum_exp(a, axis=0)
+    print(int_shape(a))
+    quit()
     lse_sum = tf.reduce_mean(log_sum_exp(tf.reduce_sum(log_probs, axis=-1), axis=0))
     sum_lse = tf.reduce_mean(tf.reduce_sum(log_sum_exp(log_probs, axis=0), axis=-1))
     return lse_sum, sum_lse
