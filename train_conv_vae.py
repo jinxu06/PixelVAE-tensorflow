ddbe729b1e6f62dd1b805c46f74ea721bc8e47ab
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-17 20:39:29 +0800

    sync

--- train_conv_vae.py
+++ train_conv_vae.py
@@ -277,9 +277,7 @@ with tf.Session(config=config) as sess:
             loss_arr.append(l)
             loss_ae_arr.append(la)
             loss_reg_arr.append(lr)
-            print(lr[:, :, 0])
-            print(lr[:, :, 1])
-            print(lr.shape)
+            print(lr)
             quit()
         train_loss, train_loss_ae, train_loss_reg = np.mean(loss_arr), np.mean(loss_ae_arr), np.mean(loss_reg_arr)
 
