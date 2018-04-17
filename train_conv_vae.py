4bcc2222c16cb82c14bd4f45dcf088d72b415681
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-17 20:19:21 +0800

    sync

--- train_conv_vae.py
+++ train_conv_vae.py
@@ -99,6 +99,24 @@ cfg = {
 }
 
 
+cfg = {
+    "img_size": 64,
+    "z_dim": 16,
+    "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
+    "save_dir": "/data/ziz/jxu/models/temp",
+    "data_set": "celeba64",
+    "batch_size": 512,
+    "nr_gpu": 2,
+    #"gpus": "4,5,6,7",
+    "learning_rate": 0.0002,
+    "beta": 10.0,
+    "lam": 0.0,
+    "save_interval": 10,
+    "reg": "tc",
+    "use_mode": "train",
+}
+
+
 
 
 parser.add_argument('-is', '--img_size', type=int, default=cfg['img_size'], help="size of input image")
