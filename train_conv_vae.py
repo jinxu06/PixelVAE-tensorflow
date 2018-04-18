2dc6c61dd437756b01ea541137ec6ea58f6bf68c
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-18 22:58:05 +0800

    sync

--- train_conv_vae.py
+++ train_conv_vae.py
@@ -282,8 +282,8 @@ with tf.Session(config=config) as sess:
         loss_arr, loss_ae_arr, loss_reg_arr = [], [], []
         for data in train_data:
             feed_dict = make_feed_dict(data, is_training=True)
-            #_, l, la, lr = sess.run([train_step, loss, loss_ae, loss_reg], feed_dict=feed_dict)
-            l, la, lr = sess.run([loss, loss_ae, loss_reg], feed_dict=feed_dict)
+            _, l, la, lr = sess.run([train_step, loss, loss_ae, loss_reg], feed_dict=feed_dict)
+            #l, la, lr = sess.run([loss, loss_ae, loss_reg], feed_dict=feed_dict)
             loss_arr.append(l)
             loss_ae_arr.append(la)
             loss_reg_arr.append(lr)
@@ -303,13 +303,13 @@ with tf.Session(config=config) as sess:
         print("test loss:{0:.3f}, test ae loss:{1:.3f}, test reg loss:{2:.3f}".format(test_loss, test_loss_ae, test_loss_reg))
         sys.stdout.flush()
 
-        # if epoch % args.save_interval == 0:
-        #     saver.save(sess, args.save_dir + '/params_' + args.data_set + '.ckpt')
-        #
-        #     data = next(test_data)
-        #     sample_x = sample_from_model(sess, data)
-        #     test_data.reset()
-        #
-        #     img_tile = plotting.img_tile(sample_x[:100], aspect_ratio=1.0, border_color=1.0, stretch=True)
-        #     img = plotting.plot_img(img_tile, title=args.data_set + ' samples')
-        #     plotting.plt.savefig(os.path.join(args.save_dir,'%s_vae_sample%d.png' % (args.data_set, epoch)))
+        if epoch % args.save_interval == 0:
+            saver.save(sess, args.save_dir + '/params_' + args.data_set + '.ckpt')
+
+            data = next(test_data)
+            sample_x = sample_from_model(sess, data)
+            test_data.reset()
+
+            img_tile = plotting.img_tile(sample_x[:100], aspect_ratio=1.0, border_color=1.0, stretch=True)
+            img = plotting.plot_img(img_tile, title=args.data_set + ' samples')
+            plotting.plt.savefig(os.path.join(args.save_dir,'%s_vae_sample%d.png' % (args.data_set, epoch)))
--- vae/conv_vae.py
+++ vae/conv_vae.py
@@ -60,12 +60,12 @@ class ConvVAE(object):
         elif reg=='tc':
             #tc = compute_tc(self.z, self.z_mu, self.z_log_sigma_sq)
             from layers import estimate_tc, estimate_dwkld, estimate_mi
-            N = 2000
+            N = 200000
             mi = estimate_mi(self.z, self.z_mu, self.z_log_sigma_sq, N=N)
             tc = estimate_tc(self.z, self.z_mu, self.z_log_sigma_sq, N=N)
             dwkld = estimate_dwkld(self.z, self.z_mu, self.z_log_sigma_sq, N=N)
             #kld = tf.reduce_mean(- 0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq - tf.square(self.z_mu) - tf.exp(self.z_log_sigma_sq), axis=-1))
-            self.loss_reg = tc + dwkld #mi + self.beta * tc + dwkld  #kld + (self.beta-1.) * tc
+            self.loss_reg = mi + self.beta * tc + dwkld  #kld + (self.beta-1.) * tc
 
         print("reg:{0}, beta:{1}, lam:{2}".format(self.reg, self.beta, self.lam))
         self.loss = self.loss_ae + self.loss_reg
