41427aae0b8d17a3153153f86cf03f32d556fd3e
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-18 20:28:00 +0800

    sync

--- test.py
+++ test.py
@@ -10,6 +10,14 @@ print("TensorFlow version: {}".format(tf.VERSION))
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
 
+def z_sampler(loc, scale):
+    name = "z_sampler"
+    with tf.variable_scope(name):
+        dist = tf.distributions.Normal(loc=0., scale=1.)
+        z = dist.sample(sample_shape=int_shape(loc), seed=None)
+        z = loc + tf.multiply(z, scale)
+        return z
+
 
 def estimate_log_probs(z, z_mu, z_log_sigma_sq, N=200000):
     z_b = tf.stack([z for i in range(batch_size)], axis=0)
@@ -29,10 +37,11 @@ def estimate_log_probs(z, z_mu, z_log_sigma_sq, N=200000):
 batch_size = 512
 z_dim = 16
 
-z_mu = np.random.normal(size=(batch_size, z_dim))
-z_log_sigma_sq = np.random.normal(size=(batch_size, z_dim))
-z_sigma = np.sqrt(np.exp(z_log_sigma_sq))
-z = np.random.normal(loc=z_mu, scale=z_sigma)
+
+z_mu = np.zeros((batch_size, z_dim), np.float32)   #np.random.normal(size=(batch_size, z_dim))
+z_log_sigma_sq = np.ones((batch_size, z_dim), dtype=np.float32)  #np.random.normal(size=(batch_size, z_dim))
+z_sigma = tf.sqrt(tf.exp(z_log_sigma_sq))
+z = z_sampler(loc=z_mu, scale=z_sigma)
 
 e = estimate_log_probs(z, z_mu, z_log_sigma_sq)
 print(e)
