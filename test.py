9253b09a55d773af5ebc00806dcf9314a6470dff
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-18 20:44:20 +0800

    sync

--- test.py
+++ test.py
@@ -32,8 +32,8 @@ def estimate_log_probs(z, z_mu, z_log_sigma_sq, N=batch_size):
 
     dist = tf.distributions.Normal(loc=0., scale=1.)
     log_probs = dist.log_prob(z_norm)
-    lse_sum = tf.reduce_mean(log_sum_exp(tf.reduce_sum(log_probs, axis=-1), axis=0))
-    sum_lse = tf.reduce_mean(tf.reduce_sum(log_sum_exp(log_probs, axis=0), axis=-1))
+    lse_sum = tf.reduce_mean(log_sum_exp(tf.reduce_sum(log_probs, axis=-1), axis=1))
+    sum_lse = tf.reduce_mean(tf.reduce_sum(log_sum_exp(log_probs, axis=1), axis=-1))
 
     print(lse_sum-sum_lse)
     print(np.log(batch_size)*(z_dim-1))
