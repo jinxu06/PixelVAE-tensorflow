2ca15ad858630ccdf50ad42f2374424708f0ffec
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-19 17:48:56 +0800

    sync

--- train_conv_vae.py
+++ train_conv_vae.py
@@ -127,7 +127,7 @@ parser.add_argument('-si', '--save_interval', type=int, default=cfg['save_interv
 parser.add_argument('-lp', '--load_params', dest='load_params', action='store_true', help='Restore training from previous model checkpoint?')
 parser.add_argument('-bs', '--batch_size', type=int, default=cfg['batch_size'], help='Batch size during training per GPU')
 parser.add_argument('-ng', '--nr_gpu', type=int, default=0, help='How many GPUs to distribute the training across?')
-parser.add_argument('-g', '--gpus', type=str, default=None, help='GPU No.s')
+parser.add_argument('-g', '--gpus', type=str, default="", help='GPU No.s')
 parser.add_argument('-lr', '--learning_rate', type=float, default=cfg['learning_rate'], help='Base learning rate')
 parser.add_argument('-b', '--beta', type=float, default=cfg['beta'], help="strength of the KL divergence penalty")
 parser.add_argument('-l', '--lam', type=float, default=cfg['lam'], help="")
