041e59f3523223060fd7b862c0d795cdc37cee75
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-19 17:35:17 +0800

    sync

--- blocks/helpers.py
+++ blocks/helpers.py
@@ -23,10 +23,19 @@ def get_name(layer_name, counters):
     counters[layer_name] += 1
     return name
 
+def tile_images(imgs, size=(6, 6)):
+    imgs = imgs[:size[0]*size[1], :, :, :]
+    img_h, img_w = imgs.shape[1], imgs.shape[2]
+    all_images = np.zeros((img_h*size[0], img_w*size[1], 3), np.uint8)
+    for j in range(size[0]):
+        for i in range(size[1]):
+            all_images[img_h*j:img_h*(j+1), img_w*i:img_w*(i+1), :] = imgs[j*size[1]+i, :, :, :]
+    return all_images
+    
 def visualize_samples(images, name="results/test.png", layout=[5,5], vrange=[-1., 1.]):
     images = (images - vrange[0]) / (vrange[1]-vrange[0]) * 255.
     images = np.rint(images).astype(np.uint8)
-    view = uf.tile_images(images, size=layout)
+    view = tile_images(images, size=layout)
     if name is None:
         return view
     view = Image.fromarray(view, 'RGB')
