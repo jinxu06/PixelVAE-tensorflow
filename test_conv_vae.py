4bd764960ba72f60311949c105344fd6107e950a
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-20 15:51:54 +0800

    sync

--- test_conv_vae.py
+++ test_conv_vae.py
@@ -262,7 +262,7 @@ with tf.Session(config=config) as sess:
     test_data.reset()
     img = []
     for i in range(3):
-        sample_x = latent_traversal(sess, data, use_image_id=10+i)
+        sample_x = latent_traversal(sess, data, use_image_id=5+i)
         view = visualize_samples(sample_x, None, layout=(args.z_dim, num_traversal_step))
         img.append(view.copy())
     img = np.concatenate(img, axis=1)
