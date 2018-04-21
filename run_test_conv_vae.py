a20b61a96252db0eaaa66cdf874d90f6acb4f0ec
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-21 16:53:32 +0800

    sync

--- run_test_conv_vae.py
+++ run_test_conv_vae.py
@@ -62,7 +62,7 @@ if args.use_mode == 'test':
 
 args.nr_gpu = len(args.gpus.split(","))
 num_traversal_step = 13
-args.batch_size = num_traversal_step * (args.z_dim+4) // args.nr_gpu
+args.batch_size = num_traversal_step * (args.z_dim) // args.nr_gpu
 args.use_mode = 'test'
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
 print('input args:\n', json.dumps(vars(args), indent=4, separators=(',',':'))) # pretty print args
@@ -163,46 +163,46 @@ def generate_samples(sess, data):
 
 
 
-def latent_traversal(sess, data, use_image_id=0):
-    data = np.cast[np.float32]((data - 127.5) / 127.5)
-    ds = np.split(data, args.nr_gpu)
-    feed_dict = {is_trainings[i]:False for i in range(args.nr_gpu)}
-    feed_dict.update({xs[i]:ds[i] for i in range(args.nr_gpu)})
-    z_mu = np.concatenate(sess.run([vaes[i].z_mu for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
-    z_log_sigma_sq = np.concatenate(sess.run([vaes[i].z_log_sigma_sq for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
-    z_sigma = np.sqrt(np.exp(z_log_sigma_sq))
-    z = np.random.normal(loc=z_mu, scale=z_sigma)
-    for i in range(z.shape[0]):
-        z[i] = z[use_image_id].copy()
-
-    num_features = args.z_dim
-    for i in range(num_features):
-        z[i*num_traversal_step:(i+1)*num_traversal_step, i] = np.linspace(start=-6., stop=6., num=num_traversal_step)
-    z = np.split(z, args.nr_gpu)
-    feed_dict.update({vaes[i].z:z[i] for i in range(args.nr_gpu)})
-    x_hats = sess.run([vaes[i].x_hat for i in range(args.nr_gpu)], feed_dict=feed_dict)
-    return np.concatenate(x_hats, axis=0)
-
-# def latent_traversal(sess, image, range=[-6, 6], num_traversal_step=13):
-#     num_instances = num_traversal_step * args.z_dim
-#     data = np.stack([image.copy() for i in range(int(np.ceil(num_instances/float(args.nr_gpu))))], axis=0)
+# def latent_traversal(sess, data, use_image_id=0):
 #     data = np.cast[np.float32]((data - 127.5) / 127.5)
 #     ds = np.split(data, args.nr_gpu)
 #     feed_dict = {is_trainings[i]:False for i in range(args.nr_gpu)}
-#     feed_dict.update({dropout_ps[i]: 0. for i in range(args.nr_gpu)})
 #     feed_dict.update({xs[i]:ds[i] for i in range(args.nr_gpu)})
-#     z_mu = np.concatenate(sess.run([pvaes[i].z_mu for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
-#     z_log_sigma_sq = np.concatenate(sess.run([pvaes[i].z_log_sigma_sq for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
+#     z_mu = np.concatenate(sess.run([vaes[i].z_mu for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
+#     z_log_sigma_sq = np.concatenate(sess.run([vaes[i].z_log_sigma_sq for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
 #     z_sigma = np.sqrt(np.exp(z_log_sigma_sq))
 #     z = np.random.normal(loc=z_mu, scale=z_sigma)
 #     for i in range(z.shape[0]):
-#         z[i] = z[0].copy()
-#     for i in range(args.z_dim):
-#         z[i*num_traversal_step:(i+1)*num_traversal_step, i] = np.linspace(start=range[0], stop=range[1], num=num_traversal_step)
+#         z[i] = z[use_image_id].copy()
+#
+#     num_features = args.z_dim
+#     for i in range(num_features):
+#         z[i*num_traversal_step:(i+1)*num_traversal_step, i] = np.linspace(start=-6., stop=6., num=num_traversal_step)
 #     z = np.split(z, args.nr_gpu)
 #     feed_dict.update({vaes[i].z:z[i] for i in range(args.nr_gpu)})
 #     x_hats = sess.run([vaes[i].x_hat for i in range(args.nr_gpu)], feed_dict=feed_dict)
-#     return np.concatenate(x_hats, axis=0)[:num_instances]
+#     return np.concatenate(x_hats, axis=0)
+
+def latent_traversal(sess, image, range=[-6, 6], num_traversal_step=13):
+    num_instances = num_traversal_step * args.z_dim
+    data = np.stack([image.copy() for i in range(int(np.ceil(num_instances/float(args.nr_gpu))))], axis=0)
+    data = np.cast[np.float32]((data - 127.5) / 127.5)
+    ds = np.split(data, args.nr_gpu)
+    feed_dict = {is_trainings[i]:False for i in range(args.nr_gpu)}
+    feed_dict.update({dropout_ps[i]: 0. for i in range(args.nr_gpu)})
+    feed_dict.update({xs[i]:ds[i] for i in range(args.nr_gpu)})
+    z_mu = np.concatenate(sess.run([pvaes[i].z_mu for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
+    z_log_sigma_sq = np.concatenate(sess.run([pvaes[i].z_log_sigma_sq for i in range(args.nr_gpu)], feed_dict=feed_dict), axis=0)
+    z_sigma = np.sqrt(np.exp(z_log_sigma_sq))
+    z = np.random.normal(loc=z_mu, scale=z_sigma)
+    for i in range(z.shape[0]):
+        z[i] = z[0].copy()
+    for i in range(args.z_dim):
+        z[i*num_traversal_step:(i+1)*num_traversal_step, i] = np.linspace(start=range[0], stop=range[1], num=num_traversal_step)
+    z = np.split(z, args.nr_gpu)
+    feed_dict.update({vaes[i].z:z[i] for i in range(args.nr_gpu)})
+    x_hats = sess.run([vaes[i].x_hat for i in range(args.nr_gpu)], feed_dict=feed_dict)
+    return np.concatenate(x_hats, axis=0)[:num_instances]
 
 
 
@@ -218,12 +218,12 @@ with tf.Session(config=config) as sess:
     print('restoring parameters from', ckpt_file)
     saver.restore(sess, ckpt_file)
 
-    data = test_data.next((args.z_dim+4)*num_traversal_step)
+    data = test_data.next(args.z_dim*num_traversal_step)
     test_data.reset()
     img = []
     for i in [2, 3, 6]:
         sample_x = latent_traversal(sess, data, use_image_id=i)
-        view = visualize_samples(sample_x, None, layout=(args.z_dim+4, num_traversal_step))
+        view = visualize_samples(sample_x, None, layout=(args.z_dim, num_traversal_step))
         img.append(view.copy())
     img = np.concatenate(img, axis=1)
     from PIL import Image
