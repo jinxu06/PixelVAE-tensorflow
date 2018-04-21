4e706f14380215f4486cb22a9eded3229561a1bc
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-21 17:03:54 +0800

    sync

--- run_test_conv_vae.py
+++ run_test_conv_vae.py
@@ -184,11 +184,11 @@ def generate_samples(sess, data):
 #     return np.concatenate(x_hats, axis=0)
 
 def latent_traversal(sess, image, range=[-6, 6], num_traversal_step=13):
-    print(type(image))
     image = np.cast[np.float32]((image - 127.5) / 127.5)
     num_instances = num_traversal_step * args.z_dim
-    data = np.stack([image.copy() for i in range(int(np.ceil(num_instances/float(args.nr_gpu))))], axis=0)
-    #data = np.cast[np.float32]((data - 127.5) / 127.5)
+    num_instances_ceil = int(np.ceil(num_instances/float(args.nr_gpu)))
+    print(num_instances_ceil)
+    data = np.stack([image.copy() for i in range(num_instances_ceil)], axis=0)
     ds = np.split(data, args.nr_gpu)
     feed_dict = {is_trainings[i]:False for i in range(args.nr_gpu)}
     feed_dict.update({dropout_ps[i]: 0. for i in range(args.nr_gpu)})
