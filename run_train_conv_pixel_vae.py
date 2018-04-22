a48df91f279036058e6dc6b07caf1780bb0aecb8
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-22 20:00:50 +0800

    sync

--- run_train_conv_pixel_vae.py
+++ run_train_conv_pixel_vae.py
@@ -317,7 +317,7 @@ with tf.Session(config=config) as sess:
         saver.restore(sess, ckpt_file)
 
     # restore part of parameters
-    var_list=get_trainable_variables(["conv_encoder", "conv_decoder"])
+    var_list=get_trainable_variables(["conv_encoder", "conv_decoder", "conv_pixel_cnn"])
     pretraining_dir = "/data/ziz/jxu/models/pvae_celeba32_z32_mmd"
     saver1 = tf.train.Saver(var_list=var_list)
     ckpt_file = pretraining_dir + '/params_' + args.data_set + '.ckpt'
