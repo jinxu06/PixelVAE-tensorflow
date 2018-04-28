5c414aa772735194efc634c11f714e56f9ccbe4e
Jin Xu <aaron.jin.xu@gmail.com>
2018-04-28 16:20:56 +0800

    sync

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-import numpy as np
 import os
+import sys
+import json
+import argparse
 import time
+import numpy as np
 import tensorflow as tf
-from layers import *
+import data.load_data as load_data
 
 import tensorflow.contrib.eager as tfe
 tfe.enable_eager_execution()
@@ -10,42 +13,9 @@ tfe.enable_eager_execution()
 print("TensorFlow version: {}".format(tf.VERSION))
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
+data_set = load_data.CelebA(data_dir=args.data_dir, batch_size=batch_size, img_size=args.img_size)
+test_data = data_set.test(shuffle=False)
 
-def z_sampler(loc, scale):
-    name = "z_sampler"
-    with tf.variable_scope(name):
-        dist = tf.distributions.Normal(loc=0., scale=1.)
-        z = dist.sample(sample_shape=int_shape(loc), seed=None)
-        z = loc + tf.multiply(z, scale)
-        return z
-
-batch_size = 512
-z_dim = 20
-
-def estimate_log_probs(z, z_mu, z_log_sigma_sq):
-    z_b = tf.stack([z for i in range(batch_size)], axis=0)
-
-    z_mu_b = tf.stack([z_mu for i in range(batch_size)], axis=1)
-    z_sigma_b = tf.stack([z_sigma for i in range(batch_size)], axis=1)
-    z_norm = (z_b-z_mu_b) / z_sigma_b
-
-
-    dist = tf.distributions.Normal(loc=0., scale=1.)
-    log_probs = dist.log_prob(z_norm)
-    lse_sum = tf.reduce_mean(log_sum_exp(tf.reduce_sum(log_probs, axis=-1), axis=0))
-    sum_lse = tf.reduce_mean(tf.reduce_sum(log_sum_exp(log_probs, axis=0), axis=-1))
-
-    print(lse_sum-sum_lse)
-    print(np.log(batch_size)*(z_dim-1))
-
-
-dist = tf.distributions.Normal(loc=0., scale=1.)
-
-z_mu = dist.sample(sample_shape=(batch_size, z_dim), seed=None) / 10. #tf.zeros((batch_size, z_dim))
-z_log_sigma_sq = tf.zeros((batch_size, z_dim))
-
-
-z_sigma = tf.sqrt(tf.exp(z_log_sigma_sq))
-z = z_sampler(loc=z_mu, scale=z_sigma)
-
-e = estimate_log_probs(z, z_mu, z_log_sigma_sq)
+data = test_data.next(256)
+data = np.cast[np.float32]((data - 127.5) / 127.5)
+visualize_samples(data, os.path.join("results", "examples.png"), layout=(16, 16))
