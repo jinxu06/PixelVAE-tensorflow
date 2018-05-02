e60fc71c656357f4aaa0793353efaa921b61a65f
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-02 22:47:01 +0800

    sync

--- run_test_conv_pixel_vae.py
+++ run_test_conv_pixel_vae.py
@@ -310,7 +310,8 @@ else:
 
 input_masks = [None for i in range(args.nr_gpu)]
 input_mgen = RandomRectangleMaskGenerator(args.img_size, args.img_size, min_ratio=1./16, max_ratio=.75)
-input_test_mgen = RectangleMaskGenerator(args.img_size, args.img_size, rec=[8, 31, 18, 1])
+input_test_mgen = RectangleMaskGenerator(args.img_size, args.img_size, rec=[20, 31, 32, 1])
+
 if "use_input_masks" in cfg and cfg["use_input_masks"]:
     input_masks = [tf.placeholder(tf.float32, shape=(args.batch_size, args.img_size, args.img_size)) for i in range(args.nr_gpu)]
 
