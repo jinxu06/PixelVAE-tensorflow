af9eb8c0f6da3dbfe8033897caed85de76537503
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-02 23:07:05 +0800

    sync

--- run_test_conv_pixel_vae.py
+++ run_test_conv_pixel_vae.py
@@ -240,6 +240,23 @@ cfg.update({
     "use_input_masks": True,
 })
 
+cfg = cfg_default
+cfg.update({
+    "img_size": 32,
+    "data_set": "celeba32",
+    "z_dim": 32,
+    "save_dir": "/data/ziz/jxu/models/pvae_celeba32_z32_mmd_medium_elu5_noise",
+    "beta": 5e5,
+    "reg": "mmd",
+    "use_mode": "test",
+    "mask_type": "full",
+    "batch_size": 104,
+    "network_size": "medium",
+    "masked": False,
+    "nonlinearity": "elu",
+    "use_input_masks": True,
+})
+
 cfg['sample_range'] = 1.0
 
 
@@ -535,4 +552,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("/data/ziz/jxu/gpu-results/show_pvae_mask_mouth_masking.png")
+    img.save("/data/ziz/jxu/gpu-results/show_pvae_mask_mouth_noise.png")
