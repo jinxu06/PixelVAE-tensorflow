be112a7dcd4e543b199b416f617984b41cdff89c
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-03 11:47:43 +0800

    sync

--- run_test_conv_pixel_vae.py
+++ run_test_conv_pixel_vae.py
@@ -557,4 +557,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("/data/ziz/jxu/gpu-results/show_pvae_mask_mouth_noise_temp_3.png")
+    img.save("/data/ziz/jxu/gpu-results/show_pvae_mask_mouth_noise_temp_4.png")
