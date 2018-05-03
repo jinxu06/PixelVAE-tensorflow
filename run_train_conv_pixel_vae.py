b0d4f9f1f9502a5727b793e63219a4f8a824a133
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-03 16:46:39 +0800

    sync

--- run_train_conv_pixel_vae.py
+++ run_train_conv_pixel_vae.py
@@ -539,6 +539,8 @@ def make_feed_dict(data, is_training=True, dropout_p=0.5, mgen=None):
     if masks[0] is not None:
         masks_np.append(mgen.gen(args.batch_size))
         feed_dict.update({masks[i]:masks_np[-1] for i in range(args.nr_gpu)})
+    print(input_masks)
+    print(len(masks_np))
     if input_masks[0] is not None:
         if 'context' in cfg['phase']:
             feed_dict.update({input_masks[i]:masks_np[i] for i in range(args.nr_gpu)})
@@ -587,8 +589,7 @@ def generate_samples(sess, data, fill_region=None, mgen=None):
     if masks[0] is not None:
         masks_np.append(mgen.gen(args.batch_size))
         feed_dict.update({masks[i]:masks_np[-1] for i in range(args.nr_gpu)})
-    print(input_masks)
-    print(len(masks_np))
+
     if input_masks[0] is not None:
         if 'context' in cfg['phase']:
             feed_dict.update({input_masks[i]:masks_np[i] for i in range(args.nr_gpu)})
