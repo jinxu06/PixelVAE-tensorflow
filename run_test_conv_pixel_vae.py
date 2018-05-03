4ba5c9005fb61ae68ab43e4c89c9a428da61e703
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-03 11:16:20 +0800

    sync

--- run_test_conv_pixel_vae.py
+++ run_test_conv_pixel_vae.py
@@ -327,7 +327,7 @@ else:
 
 input_masks = [None for i in range(args.nr_gpu)]
 input_mgen = RandomRectangleMaskGenerator(args.img_size, args.img_size, min_ratio=1./16, max_ratio=.75)
-input_test_mgen = RectangleMaskGenerator(args.img_size, args.img_size, rec=[20, 31, 32, 1])
+input_test_mgen = CenterMaskGenerator(args.img_size, args.img_size, ratio=0.)
 # CenterMaskGenerator(args.img_size, args.img_size, ratio=0.)
 # RectangleMaskGenerator(args.img_size, args.img_size, rec=[20, 31, 32, 1])
 
@@ -557,4 +557,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("/data/ziz/jxu/gpu-results/show_pvae_mask_mouth_noise_temp.png")
+    img.save("/data/ziz/jxu/gpu-results/show_pvae_mask_mouth_noise_temp_1.png")
