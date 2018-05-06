08c90f6153fafdc95b494f5ba8b03244aaa29bcb
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-06 16:16:21 +0800

    sync

--- run_test_conv_pixel_vae.py
+++ run_test_conv_pixel_vae.py
@@ -25,14 +25,13 @@ parser = argparse.ArgumentParser()
 # config = {"nonlinearity": "elu", "nr_resnet":1, "reg":'kld', "beta":1.0, "batch_size": 32, "sample_range":1.}
 # cfg = get_config(config=config, name=None, suffix="", load_dir=None, dataset='celeba', size=32, mode='test', phase='pvae', use_mask_for="input output")
 
-# large network, bn before nonlinearity, beta 1d5
-config = {"nonlinearity": "elu", "network_size":"large", "beta":1e5, "batch_size": 104, "sample_range":1.}
-cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='test', phase='pvae', use_mask_for="input output")
-
-
-# # large network, bn before nonlinearity, beta 5e5
-# config = {"nonlinearity": "elu", "network_size":"large", "beta":5e5, "batch_size": 104, "sample_range":1.}
+# # large network, bn before nonlinearity, beta 1d5
+# config = {"nonlinearity": "elu", "network_size":"large", "beta":1e5, "batch_size": 104, "sample_range":1.}
 # cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='test', phase='pvae', use_mask_for="input output")
+
+# large network, bn before nonlinearity, beta 5e5
+config = {"nonlinearity": "elu", "network_size":"large", "beta":5e5, "batch_size": 104, "sample_range":1.}
+cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='test', phase='pvae', use_mask_for="input output")
 
 
 # # # large network, mmd-tc
@@ -344,4 +343,4 @@ with tf.Session(config=config) as sess:
     from PIL import Image
     img = img.astype(np.uint8)
     img = Image.fromarray(img, 'RGB')
-    img.save("/data/ziz/jxu/gpu-results/mouth_completion_b1.png")
+    img.save("/data/ziz/jxu/gpu-results/mouth_completion_b5.png")
