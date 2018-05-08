6637319ee947f898cc71489aabcb5c850eb962b1
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-08 15:25:06 +0800

    sync

--- run_train_conv_pixel_vae.py
+++ run_train_conv_pixel_vae.py
@@ -212,8 +212,12 @@ cfg_default = {
 # config = {"nonlinearity": "elu", "network_size":"large", "beta":1e6, "nr_resnet":3}
 # cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='train', phase='pvae', use_mask_for="input output")
 
-# large network, bn before nonlinearity, beta 5e6, nr_resnet 5
-config = {"nonlinearity": "elu", "network_size":"large", "beta":5e6, "nr_resnet":5}
+# # large network, bn before nonlinearity, beta 5e6, nr_resnet 5
+# config = {"nonlinearity": "elu", "network_size":"large", "beta":5e6, "nr_resnet":5}
+# cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='train', phase='pvae', use_mask_for="input output")
+
+# large network, bn before nonlinearity, beta 2e6, nr_resnet 5
+config = {"nonlinearity": "elu", "network_size":"large", "beta":2e6, "nr_resnet":5}
 cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='train', phase='pvae', use_mask_for="input output")
 
 
