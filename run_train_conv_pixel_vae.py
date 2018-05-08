3e6671695d126bc3d8a73b209fd6ef7be0897265
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-08 14:59:42 +0800

    sync

--- run_train_conv_pixel_vae.py
+++ run_train_conv_pixel_vae.py
@@ -208,6 +208,9 @@ cfg_default = {
 # config = {"nonlinearity": "elu", "network_size":"large", "beta":0.1, 'reg':'mmd-tc'}
 # cfg = get_config(config=config, name=None, suffix="_test", load_dir=None, dataset='celeba', size=32, mode='train', phase='pvae', use_mask_for="input output")
 
+# large network, bn before nonlinearity, beta 1e6
+config = {"nonlinearity": "elu", "network_size":"large", "beta":1e6, "nr_resnet":3}
+cfg = get_config(config=config, name=None, suffix="_large", load_dir=None, dataset='celeba', size=32, mode='train', phase='pvae', use_mask_for="input output")
 
 
 parser.add_argument('-is', '--img_size', type=int, default=cfg['img_size'], help="size of input image")
