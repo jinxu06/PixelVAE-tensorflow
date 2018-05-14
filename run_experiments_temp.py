4ab6c5c158e44ecd9f70a80528fef1f8e3789735
Jin Xu <aaron.jin.xu@gmail.com>
2018-05-14 11:01:00 +0800

    sync

--- run_experiments_temp.py
+++ run_experiments_temp.py
@@ -507,11 +507,11 @@ with tf.Session(config=config) as sess:
         data[i] = data[-1].copy()
     test_data.reset()
     gt_data = np.cast[np.float32]((data - 127.5) / 127.5)
-    sample_mgen = get_generator('center', args.img_size)
+    sample_mgen = get_generator('eye', args.img_size)
     fill_region = sample_mgen.gen(1)[0]
     sample_x = generate_samples(sess, data, fill_region=fill_region, mgen=sample_mgen)
-    visualize_samples(gt_data, "/data/ziz/jxu/gpu-results/recon_gt_center_mmd_2e6_randomz.png", layout=(4,4))
-    visualize_samples(sample_x, "/data/ziz/jxu/gpu-results/recon_sample_center_mmd_2e6_randomz.png", layout=(4,4))
+    visualize_samples(gt_data, "/data/ziz/jxu/gpu-results/recon_gt_eye_mmd_2e6_randomz.png", layout=(4,4))
+    visualize_samples(sample_x, "/data/ziz/jxu/gpu-results/recon_sample_eye_mmd_2e6_randomz.png", layout=(4,4))
 
 quit()
 
