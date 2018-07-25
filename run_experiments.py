7bdddb3608496971d0815bd7276a987ed1927642
Jin Xu <aaron.jin.xu@gmail.com>
2018-07-25 17:15:52 +0800

    sync

--- run_experiments.py
+++ run_experiments.py
@@ -228,7 +228,7 @@ if args.mode == 'train':
             record_dict['mmd'] = tf.add_n([v.mmd for v in pvaes]) / args.nr_gpu
             record_dict['mmdtc'] = tf.add_n([v.mmdtc for v in pvaes]) / args.nr_gpu
         else:
-            raise Exception("unknown reg type")
+            raise Exception("unknown reg type: {0}".format(args.reg))
         recorder = Recorder(dict=record_dict, config_str=str(json.dumps(vars(args), indent=4, separators=(',',':'))), log_file=args.save_dir+"/log_file")
         train_step = adam_updates(all_params, grads[0], lr=args.learning_rate)
 
